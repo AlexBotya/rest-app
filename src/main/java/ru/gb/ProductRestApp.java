d809c61bcde9491761c8bf817c28e948473d8c1b
AlexBotya <alex_balg@bk.ru>
2021-09-28 00:26:17 +0300

    Lesson 9 ready/ smth going wrong when delete mapping: Resolved [org.springframework.web.HttpMediaTypeNotAcceptableException: Could not find acceptable representation]

---
+++ src/main/java/ru/gb/ProductRestApp.java
@@ -0,0 +1,11 @@
+package ru.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRestApp.class, args);
+    }
+}
